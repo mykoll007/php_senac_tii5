c5d877167f117349fbb6445bb7a749a4e6dab7eb
mykoll007 <mykoll.daniel@gmail.com>
2024-06-04 20:44:31 -0300

    Classe conta generica

---
+++ 00/script.js
@@ -0,0 +1,26 @@
+class Conta {
+    constructor(numero, saldo = 0){
+        this.numero = numero;
+        this.saldo = saldo;
+    }
+
+    sacar(valor){
+        if(this.saldo >= valor && valor > 0){
+            this.saldo -= valor;
+        }
+        else {
+            console.log("Saldo insuficiente") 
+        }
+    }
+
+    depositar(valor){
+        if(valor > 0){
+            this.saldo += valor;
+        }
+    }
+
+    toString(){
+        return `Conta: ${this.numero} - Saldo: ${this.saldo}  `
+    }
+
+}

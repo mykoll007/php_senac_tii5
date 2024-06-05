d2fb500cd16b3f59580418cb0948f84c31d2ebcc
mykoll007 <mykoll.daniel@gmail.com>
2024-06-04 22:02:20 -0300

    Conta corrente e poupança adicionado

--- 00/script.js
+++ 00/script.js
@@ -1,26 +1,63 @@
 class Conta {
-    constructor(numero, saldo = 0){
+    constructor(numero, saldo = 0) {
         this.numero = numero;
         this.saldo = saldo;
     }
 
-    sacar(valor){
-        if(this.saldo >= valor && valor > 0){
+    sacar(valor) {
+        if(this.saldo >= valor && valor > 0) {
             this.saldo -= valor;
-        }
-        else {
-            console.log("Saldo insuficiente") 
+        } else {
+            console.log("Saldo insuficiente");
         }
     }
 
-    depositar(valor){
-        if(valor > 0){
+    depositar(valor) {
+        if(valor > 0) {
             this.saldo += valor;
         }
     }
 
-    toString(){
-        return `Conta: ${this.numero} - Saldo: ${this.saldo}  `
+    toString() {
+        return `Numero: ${this.numero} - Saldo: ${this.saldo}`;
+    }
+}
+
+class ContaCorrente extends Conta {
+    constructor(numero, saldo = 0, limiteChequeEspecial = 200) {
+        super(numero, saldo);
+        this.limiteChequeEspecial = limiteChequeEspecial;
     }
 
+    sacar(valor) {
+        //super.sacar(valor);
+        const valorDisponivelSaque = this.saldo + this.limiteChequeEspecial;
+
+        if(valor > 0 && valorDisponivelSaque >= valor) {
+            this.saldo -= valor;
+        } else {
+            console.log("Saldo insuficiente");
+        }
+    }
 }
+
+class ContaPoupanca extends Conta {    
+    constructor(numero, saldo = 0, taxaRendimento = 0.2) {
+        super(numero, saldo)
+        this.taxaRendimento = taxaRendimento;
+    }
+
+    aplicarRendimento() {
+        console.log("Rendimento aplicado");
+    }
+}
+
+let contaC = new ContaCorrente(2001, 500);
+let contaP = new ContaPoupanca(2002, 500);
+
+console.log(contaC.toString());
+contaC.sacar(650);
+console.log(contaC.toString());
+
+contaP.aplicarRendimento();
+//contaC.aplicarRendimento(); # erro por não existir função nessa classe

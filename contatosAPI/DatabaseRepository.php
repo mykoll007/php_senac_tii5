585689fc23f7594c49c474c32972368fe3670930
MYKOLL DANIEL CUNHA GUIMARAES <mykoll.dcguimaraes@senacsp.edu.br>
2024-08-20 22:04:02 -0300

    contatos api

---
+++ contatosAPI/DatabaseRepository.php
@@ -0,0 +1,43 @@
+<?php
+class DatabaseRepository{
+    private static $dsn = 'mysql:host=localhost;dbname=contatos';
+    private static $username = 'root';
+    private static $password = '';  
+
+    private static function connect(){
+        try{
+            $pdo = new PDO(self::$dsn, self::$username, self::$password);
+            $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+            return $pdo;
+        } 
+        catch(PDOException $e){
+            echo 'Falha na conexão: ' . $e->getMessage();
+            exit;
+        }
+    }
+
+    public static function getAllContacts() {
+        $pdo = self::connect();
+        $sql = "SELECT * FROM contatos_info";
+        $stmt = $pdo->query($sql);
+        $result = $stmt->fetchAll(PDO::FETCH_ASSOC);
+        return $result;
+    }
+    public static function getContactById($id) {
+        $pdo = self::connect();
+        $sql = "SELECT * FROM contatos_info WHERE id = :id";
+        $stmt = $pdo->prepare($sql);
+        $stmt ->execute(['id' => $id]);
+        return $stmt->fetch(PDO::FETCH_ASSOC);
+        
+    }
+
+    public static function insertContact($nome, $telefone, $email){
+        $pdo = self::connect();
+        $sql = "INSERT INTO contatos_info (nome, telefone, email) VALUES (:nome, :telefone, :email)";
+        $stmt = $pdo->prepare($sql);
+        return $stmt->execute(['nome' => $nome, 'telefone' => $telefone, 'email' => $email]);
+    }
+}
+
+?>
---
+++ contatosAPI/api.php
@@ -0,0 +1,36 @@
+<?php
+
+require_once 'DatabaseRepository.php';
+
+// $action;
+
+// if(isset($_GET['action'])) {
+//     $action = $_GET['action'];
+// } else {
+//     $action = '';
+// }
+//Mesma coisa em cima ou aqui embaixo na linha 13
+$action = isset($_GET['action']) ? $_GET['action'] : '';
+
+
+
+switch($action){
+    case 'list':
+    echo json_encode(DatabaseRepository::getAllContacts());
+    break;
+    case 'get':
+        $id = $_GET['id'];
+        echo json_encode(DatabaseRepository::getContactById($id));
+        break;
+        case 'add':
+            $data = json_decode(file_get_contents('php://input', true));
+            $success = DatabaseRepository::insertContact($data->nome, $data->telefone, $data->email);
+            echo json_encode($success);
+            break;
+    default:
+    echo json_encode(['error' => 'Acao invalida']);
+}
+        
+
+
+?>
